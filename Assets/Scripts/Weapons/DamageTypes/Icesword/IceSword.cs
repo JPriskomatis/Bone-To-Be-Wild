d2bb2c8817bf1db0693e7dbbcfc2327cf1a2346f
Jason <JPriskomatis@gmail.com>
2024-08-13 18:05:25 +0300

    Add: Ice sword particle effect

--- Assets/Scripts/Weapons/DamageTypes/Icesword/IceSword.cs
+++ Assets/Scripts/Weapons/DamageTypes/Icesword/IceSword.cs
@@ -6,11 +6,24 @@ namespace WeaponSpace
 {
     public class IceSword : Weapon_base
     {
+        [SerializeField] private GameObject particlePrefab;
+
         public IceSword()
         {
             damage = 15;
             damageType = new IceDamage();
         }
+
+        private void Start()
+        {
+            if (damageType is IceDamage iceDamage)
+            {
+                iceDamage.particlePrefab = particlePrefab;
+                iceDamage.swordTransform = transform;
+
+            }
+        }
+
     }
 
 }
