3e0b3cdd4f95c94bab55cf416b9b78263f6d0bb0
Jason <JPriskomatis@gmail.com>
2024-08-13 17:38:35 +0300

    Add: Pickup weapon script

--- Assets/Scripts/Interaction/PickUpWeapon.cs
+++ Assets/Scripts/Interaction/PickUpWeapon.cs
@@ -1,26 +1,60 @@
 using System.Collections;
 using System.Collections.Generic;
+using UI;
 using UnityEngine;
 
 namespace Interaction
 {
-    public class PickUpWeapon : MonoBehaviour
+    public class PickUpWeapon : MonoBehaviour, IInteractable
     {
 
         [SerializeField] private GameObject weapon;
-        private void OnEnable()
-        {
-            Test_Object.OnWeaponPickUp += WeaponPickUp;
-        }
 
-        private void OnDisable()
-        {
-            Test_Object.OnWeaponPickUp += WeaponPickUp;
-        }
+        [SerializeField] private GameObject weaponHolder;
 
+        private GameObject CheckForActiveWeapon()
+        {
+            foreach (Transform child in weaponHolder.transform)
+            {
+                if (child.gameObject.activeInHierarchy)
+                {
+                    return child.gameObject;
+                }
+            }
+            return null;
+        }
         private void WeaponPickUp()
         {
-            weapon.SetActive(true);
+            GameObject activeWeapo = CheckForActiveWeapon();
+            if(activeWeapo != null)
+            {
+                activeWeapo.SetActive(false);
+                weapon.SetActive(true);
+            }
+            else
+            {
+                weapon.SetActive(true);
+            }
+
+        }
+
+        public void Interact()
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                WeaponPickUp();
+                Destroy(gameObject);
+            }
+        }
+
+        public void OnInteractEnter()
+        {
+            TextAppear.SetText("Interact");
+        }
+
+        public void OnInteractExit()
+        {
+            TextAppear.SetText("End of Interaction");
         }
     }
 
