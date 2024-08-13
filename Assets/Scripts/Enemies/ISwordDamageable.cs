d3b4a2ec09f3c528e333a450ef4597e4147898c1
Jason <JPriskomatis@gmail.com>
2024-08-13 18:47:05 +0300

    Update: Code clean up & documentation

---
+++ Assets/Scripts/Enemies/ISwordDamageable.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Damageables
+{
+
+    public interface ISwordDamageable
+    {
+        void SwordDamageable();
+
+    }
+    //TODO:
+    //When I implement health system, make this itnerface deal damage
+    //directly to the health of the enemy;
+
+}
